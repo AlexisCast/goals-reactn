80ab278b309ced3464eb30c6bc24236f7c20bde7
Alexis Castellanos <alexiscast@Alexiss-MacBook-Pro-2.local>
2023-09-17 13:11:47 -0700

    input container setup

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder="Your course goal!"
+				/>
+				<Button title="Add Goal" />
+			</View>
+			<View>
+				<Text>List of goals...</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	appContainer: {
+		paddingTop: 60,
+		paddingHorizontal: 16,
+	},
+	inputContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		paddingBottom: 24,
+		borderBottomWidth: 1,
+		borderColor: "grey",
+	},
+	textInput: {
+		borderWidth: 1,
+		borderColor: "grey",
+		width: "70%",
+		marginRight: 8,
+		padding: 8,
+	},
 });
