2dca81fbc4bcd1624190ac7cd5b0b4712aedee00
Alexis Castellanos <alexiscast@Alexiss-MacBook-Pro-2.local>
2023-09-17 13:33:14 -0700

    add handling events

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+	const [enteredGoalText, setEnterGoalText] = useState("");
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setEnterGoalText(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		setEnterGoalText("");
+		setCourseGoals((prev) => [...prev, enteredGoalText]);
+	};
+
 	return (
 		<View style={styles.appContainer}>
 			<View style={styles.inputContainer}>
 				<TextInput
 					style={styles.textInput}
 					placeholder="Your course goal!"
+					onChangeText={goalInputHandler}
+					value={enteredGoalText}
 				/>
-				<Button title="Add Goal" />
+				<Button title="Add Goal" onPress={addGoalHandler} />
 			</View>
 			<View style={styles.goalsContainer}>
 				<Text>List of goals...</Text>
+				{courseGoals.map((goal) => (
+					<Text key={goal}>{goal}</Text>
+				))}
 			</View>
 		</View>
 	);
